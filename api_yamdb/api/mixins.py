c1e2bea3a5e23c219b6af624b6f45d071986a4a1
Mangekos <dkvitkovsky@rocketmail.com>
2023-06-27 22:26:24 +0300

    Добавил авторизацию и приватность

---
+++ api_yamdb/api/mixins.py
@@ -0,0 +1,19 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import mixins, viewsets, filters
+from rest_framework.pagination import PageNumberPagination
+
+from .permissions import IsAdminOrReadOnly
+
+
+class CreateDestroyListViewSet(
+    mixins.CreateModelMixin,
+    mixins.DestroyModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet
+):
+
+    permission_classes = [IsAdminOrReadOnly]
+    pagination_class = PageNumberPagination
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
+    search_fields = ('name')
+    lookup_field = 'slug'
