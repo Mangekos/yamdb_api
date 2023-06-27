affa4ff5a7a379f7093f1fd44c6f1157b318e140
Максим Соловьев <maximnoch@gmail.com>
2023-06-27 22:23:42 +0300

    Вьюсеты и сериализеры

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+from reviews.models import Comment, Review
+from rest_framework.validators import UniqueTogetherValidator
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    """Обработка отзывов."""
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username'
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Review
+        read_only_fields = ('title', )
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Review.objects.all(),
+                fields=('author', 'title')
+            )
+        ]
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    """Обработка комментариев к отзывам."""
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username'
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Comment
+        read_only_fields = ('review', )
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import ReviewSerializer, CommentSerializer
+from django.shortcuts import get_object_or_404
+from .permissions import AuthorOrReadOnly
+from reviews.models import Review, Title
 
-# Create your views here.
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    """Вьюсет для отзывов."""
+    serializer_class = ReviewSerializer
+    permission_classes = (AuthorOrReadOnly,)
+
+    def get_queryset(self):
+        title_id = self.kwargs.get("title_id")
+        new_queryset = get_object_or_404(Title, id=title_id)
+        return new_queryset.reviews.all()
+
+    def perform_create(self, serializer):
+        title_id = self.kwargs.get("title_id")
+        title = get_object_or_404(Title, id=title_id)
+        serializer.save(author=self.request.user, title=title)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """Вьюсет для комментариев."""
+    serializer_class = CommentSerializer
+    permission_classes = (AuthorOrReadOnly,)
+
+    def get_queryset(self):
+        review_id = self.kwargs.get("review_id")
+        new_queryset = get_object_or_404(Review, id=review_id)
+        return new_queryset.comments.all()
+
+    def perform_create(self, serializer):
+        review_id = self.kwargs.get("review_id")
+        review = get_object_or_404(Review, id=review_id)
+        serializer.save(author=self.request.user, review=review)
